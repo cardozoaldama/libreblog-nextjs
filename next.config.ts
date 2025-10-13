72fc20ec93c4c59b83e18929cb8c05b1a0fc201c
IsmaNov12 <170652278+IsmaNov12@users.noreply.github.com>
2025-10-13 03:33:18 -0300

    feat: Use unoptimized true to solve image problems in Vercel, toy cansao

--- next.config.ts
+++ next.config.ts
@@ -2,32 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'preview.redd.it',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.redd.it',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
